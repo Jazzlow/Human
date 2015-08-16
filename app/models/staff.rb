bedf5257b5b22f7b1f9da2b800cc3da89ba8561d
murphyk9 <murphyk9@tcd.ie>
2015-08-17 00:51:12 +0300

    the final

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -3,14 +3,11 @@ belongs_to :admin
 # It returns the articles whose titles contain one or more words that form the query
   def self.search(query)
     # where(:FullName, query) -> This would return an exact match of the query
-    where("FullName like ?", "%#{query}%") 
+    where("full_name like ?", "%#{query}%") 
 
 
 
-def self.search(query)
-    
-    where("FullName like ?", "%#{query}%") 
-  end
+
 
   
 
